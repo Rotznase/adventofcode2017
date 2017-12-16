d3096e5cc3057d53703e9a5e9d8f528127379b7e
Gerd Streubel <gst@pixel.de>
2017-12-16 01:31:31 +0100

    Day15, Part 2

--- src/Day15.java
+++ src/Day15.java
@@ -1,12 +1,33 @@
 public class Day15 extends AdventOfCode {
     @Override
     void run() {
-        Generator generatorA = new Generator(16807, 699);
-        Generator generatorB = new Generator(48271, 124);
+//        int inputA = 65;
+//        int inputB = 8921;
+        int inputA = 699;
+        int inputB = 124;
+        Generator generatorA;
+        Generator generatorB;
 
+        // Teil 1
+        generatorA = new Generator(16807, inputA, 1);
+        generatorB = new Generator(48271, inputB, 1);
         int matchCounter = 0;
         for (int i=0; i<40_000_000; i++) {
-            if (generatorA.nextValue16().equals(generatorB.nextValue16()))
+            generatorA.next();
+            generatorB.next();
+            if (generatorA.first16Bit().equals(generatorB.first16Bit()))
+                matchCounter++;
+        }
+        System.out.println(matchCounter);
+
+        // Teil 2
+        generatorA = new Generator(16807, inputA, 4);
+        generatorB = new Generator(48271, inputB, 8);
+        matchCounter = 0;
+        for (int i=0; i<5_000_000; i++) {
+            generatorA.next();
+            generatorB.next();
+            if (generatorA.first16Bit().equals(generatorB.first16Bit()))
                 matchCounter++;
         }
         System.out.println(matchCounter);
@@ -15,14 +36,25 @@ public class Day15 extends AdventOfCode {
     private class Generator {
         long factor;
         long value;
+        int filter;
 
-        Generator(int factor, int start) {
+        Generator(int factor, int start, int filter) {
             this.factor = factor;
             this.value = start;
+            this.filter = filter;
         }
 
-        String nextValue16() {
-            value = value * factor % 2147483647;
+        void next() {
+            do {
+                value = value * factor % 2147483647;
+            } while (value % filter != 0);
+        }
+
+        int getValue() {
+            return (int) value;
+        }
+
+        String first16Bit() {
             return Integer.toBinaryString((int) value & 0xFFFF);
         }
     }
