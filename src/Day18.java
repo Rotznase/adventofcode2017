9009d3f762ed69cebc58302ab68fc07cff6f428f
Gerd Streubel <gst@pixel.de>
2017-12-21 23:46:10 +0100

    Day18, Part 2

--- src/Day18.java
+++ src/Day18.java
@@ -1,86 +1,164 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 public class Day18 extends AdventOfCode {
     @Override
     void run() {
 
         // Teil 1
+        Program program = new Program(0, 1, true);
+        program.run();
+        System.out.println(program.lastSound);
 
-        char register;
-        long value;
-        long lastSound = 0;
+        // Teil 2
+        Program p0 = new Program(0, 2, true);
+        Program p1 = new Program(1, 2, false);
 
-        Map<Character, Long> registerValues = new HashMap<>();
+        p0.other = p1;
+        p1.other = p0;
+
+        try {
+            if (p0.master) {
+                p0.run();
+            } else if (p1.master) {
+                p1.run();
+            }
+        } catch (RuntimeException e) {
+            if (!e.getMessage().equals("Deadlock detected"))
+                throw e;
+        }
+
+        System.out.println(p1.messageCounter);
+    }
+
+    private class Program implements Runnable {
+
+        long programId;
+        long lastSound;
+        int mode;
+
+        Queue<Long> queue;
+        Program other;
+
+        long messageCounter;
+        Map<Character, Long> registerValues;
+
+        boolean master;
         int i=0;
-        boolean stop = false;
-        while (!stop) {
-            String instruction = input[i];
-            String cmd = instruction.substring(0, 3);
-            switch (cmd) {
-                case "snd":
-                    register = instruction.charAt(4);
-                    lastSound = registerValues.get(register);
-                    i++;
-                    break;
-                case "set":
-                    long[] args = getOpArgs(instruction, registerValues);
-                    register = instruction.charAt(4);
-                    registerValues.put(register, args[1]);
-                    i++;
-                    break;
-                case "add":
-                    args = getOpArgs(instruction, registerValues);
-                    register = instruction.charAt(4);
-                    registerValues.put(register, args[0] + args[1]);
-                    i++;
-                    break;
-                case "mul":
-                    args = getOpArgs(instruction, registerValues);
-                    register = instruction.charAt(4);
-                    registerValues.put(register, args[0] * args[1]);
-                    i++;
-                    break;
-                case "mod":
-                    args = getOpArgs(instruction, registerValues);
-                    register = instruction.charAt(4);
-                    registerValues.put(register, args[0] % args[1]);
-                    i++;
-                    break;
-                case "rcv":
-                    register = instruction.charAt(4);
-                    value = registerValues.getOrDefault(register, 0L);
-                    if (value != 0) {
-                        registerValues.put(register, lastSound);
-                        stop = true;
-                    }
-                    i++;
-                    break;
-                case "jgz":
-                    register = instruction.charAt(4);
-                    value = registerValues.getOrDefault(register, 0L);
-                    if (value > 0) {
-                        args = getOpArgs(instruction, registerValues);
-                        i += args[1];
-                    } else {
-                        i++;
-                    }
-                    break;
+
+        Program(long programId, int mode, boolean master) {
+            this.programId = programId;
+            this.queue = new LinkedList<>();
+            this.other = null;
+            this.messageCounter = 0;
+            this.mode = mode;
+            this.master = master;
+
+            registerValues = new HashMap<>();
+            if (mode == 2) {
+                registerValues.put('p', programId);
             }
         }
 
-        System.out.println(registerValues);
-        System.out.println(lastSound);
+        @Override
+        public void run() {
+            char register;
+            boolean stop = false;
+            while (!stop) {
+                String instruction = input[i];
+                String cmd = instruction.substring(0, 3);
+                switch (cmd) {
+                    case "set":
+                        long[]  args = getOpArgs(instruction, registerValues);
+                        register = instruction.charAt(4);
+                        registerValues.put(register, args[1]);
+                        i++;
+                        break;
+                    case "add":
+                        args = getOpArgs(instruction, registerValues);
+                        register = instruction.charAt(4);
+                        registerValues.put(register, args[0] + args[1]);
+                        i++;
+                        break;
+                    case "mul":
+                        args = getOpArgs(instruction, registerValues);
+                        register = instruction.charAt(4);
+                        registerValues.put(register, args[0] * args[1]);
+                        i++;
+                        break;
+                    case "mod":
+                        args = getOpArgs(instruction, registerValues);
+                        register = instruction.charAt(4);
+                        registerValues.put(register, args[0] % args[1]);
+                        i++;
+                        break;
+                    case "jgz":
+                        args = getOpArgs(instruction, registerValues);
+                        i += args[0] > 0 ? args[1] : 1;
+                        break;
+                    case "snd":
+                        args = getOpArgs(instruction, registerValues);
+                        lastSound = args[0];
+                        if (mode == 2) {
+                            other.queue.add(lastSound);
+                            messageCounter++;
+                        }
+                        i++;
+                        break;
+                    case "rcv":
+                        register = instruction.charAt(4);
+
+                        if (mode == 1) {
+                            args = getOpArgs(instruction, registerValues);
+                            if (args[0] != 0) {
+                                registerValues.put(register, lastSound);
+                                stop = true;
+                            }
+                        }
+                        if (mode == 2) {
+                            if (!this.queue.isEmpty()) {
+                                registerValues.put(register, this.queue.poll());
+                            } else {
+                                if (master) {
+                                    other.run();
+                                    if (!this.queue.isEmpty()) {
+                                        registerValues.put(register, this.queue.poll());
+                                    } else {
+                                        throw new RuntimeException("Deadlock detected");
+                                    }
+                                } else {
+                                    return;
+                                }
+                            }
+                        }
+                        i++;
+                        break;
+                }
+
+                if (i >= input.length) {
+                    stop = true;
+                }
+            }
+        }
     }
 
+
     private long[] getOpArgs(String instruction, Map<Character, Long> registerValues) {
-        char register = instruction.charAt(4);
         long [] args = new long[2];
-        args[0]   = registerValues.getOrDefault(register, 0L);
-        if (Character.isAlphabetic(instruction.charAt(6))) {
-            args[1] = registerValues.getOrDefault(instruction.charAt(6), 0L);
+        if (Character.isAlphabetic(instruction.charAt(4))) {
+            char register = instruction.charAt(4);
+            args[0] = registerValues.getOrDefault(register, 0L);
         } else {
-            args[1] = Integer.parseInt(instruction.substring(6));
+            args[0] = Long.parseLong(""+instruction.charAt(4)); //todo this only scans 1-Digit Numbers!
+        }
+        if (instruction.length() > 6) {
+            if (Character.isAlphabetic(instruction.charAt(6))) {
+                args[1] = registerValues.getOrDefault(instruction.charAt(6), 0L);
+            } else {
+                args[1] = Integer.parseInt(instruction.substring(6));
+            }
         }
         return args;
     }
@@ -96,6 +174,14 @@ public class Day18 extends AdventOfCode {
 //            "jgz a -1",
 //            "set a 1",
 //            "jgz a -2",
+
+//            "snd 1",
+//            "snd 2",
+//            "snd p",
+//            "rcv a",
+//            "rcv b",
+//            "rcv c",
+//            "rcv d",
 
             "set i 31",
             "set a 1",
